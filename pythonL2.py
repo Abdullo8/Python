821856dc94410ee1d5ee9d43fc098f7ca678a5e1
Dushanbe Student <dushanbe.student@UCA.corp>
2023-04-14 20:06:15 +0500

    created new PythonL6

---
+++ pythonL2.py
@@ -0,0 +1,59 @@
+pythonStudentList = ["Abdullo", "Sadriddin", "Said", "Ali", "Ahmad", "Muhammad"]
+
+for x in pythonStudentList:
+    print(x)
+    
+
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# pythonStudentList = ["Abdullo", "Sadriddin", "Said", "Ali", "Ahmad", "Muhammad"]
+
+# pythonStudentList[1:4] = ["Parviz", "Yusuf", "Aziz"]
+# pythonStudentList[1:3] = ["UCA1","UCA2"]
+# pythonStudentList.insert(2, "UCA")
+
+# print(pythonStudentList[1]) 
+# print(len(pythonStudentList)) 
+# print(pythonStudentList[2:5])
+# print(pythonStudentList[2:])
+# print(pythonStudentList[:2])
+# print(pythonStudentList[2:])
+# print(pythonStudentList[-2])
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# pythonStudentList.append("Hello")
+# pythonStudentList.extend(pythonTeacherList)
+# pythonStudentList.pop(4)
+# pythonStudentList.remove("Abdullo")
+# pythonStudentList.pop()
+
+# pythonStudentList = ["Abdullo", "Sadriddin", "Said", "Ali", "Ahmad", "Muhammad"]
+# pythonTeacherList = ["Teacher1", "Teacher2"]
+
+# print(pythonStudentList)
+ 
+# --------------------------------------------------------------------------------------------------------------------------
+
+# pythonStudentList = ["Abdullo", "Sadriddin", "Said", "Ali", "Ahmad", "Muhammad"]
+
+# # del pythonStudentList
+# pythonStudentList.clear()
+
+# print(pythonStudentList)
+
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# FOR LOOP
+
+# pythonStudentList = ["Abdullo", "Sadriddin", "Said", "Ali", "Ahmad", "Muhammad"]
+
+# for x in pythonStudentList:
+#     print(x)
