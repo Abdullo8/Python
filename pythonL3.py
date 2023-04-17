e36012d96f1e85e780c2bd18123b7ae3e9a29525
Dushanbe Student <dushanbe.student@UCA.corp>
2023-04-17 19:57:41 +0500

    new lesson

---
+++ pythonL3.py
@@ -0,0 +1,82 @@
+# myCarInfo = {
+#     "brand":"BMW",
+#     "model":"M5 F90",
+#     "year":2022
+# }
+
+# print(myCarInfo)
+# print(myCarInfo["model"])
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+# myInfo = {
+#     "city":"Dushanbe",
+#     "education":"School №55",
+#     "birthday":"08.10.05"
+# }
+
+# print(myInfo["city"])
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# myInfo = {
+#     "city":"Dushanbe",
+#     "education":"School №55",
+#     "birthday":"08.10.05"
+# }
+
+# myInfo["birthday"]="08.10.04"
+# myInfo["city"]="Kulob"
+# myInfo["education"]="School №20"
+
+# print(myInfo)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# myInfo = {
+#     "city":"Dushanbe",
+#     "education":"School №55",
+#     "birthday":"08.10.05"
+# }
+
+# # myInfo.pop("birthday")
+# myInfo.popitem()
+
+# print(myInfo)
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+# myCarInfo = {
+#     "brand":"BMW",
+#     "model":"M5 F90",
+#     "year":2022
+# }
+
+# for x in myCarInfo.keys():
+#     print(x)
+
+# for x in myCarInfo.values():
+#     print(x)
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+# myCarInfo = {
+#     "brand":"BMW",
+#     "model":"M5 F90",
+#     "year":2022
+# }
+
+# for x , y in myCarInfo.items():
+#     print(x, y)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+myCarInfo = {
+    "brand":"BMW",
+    "model":"M5 F90",
+    "year":2022
+}
+
+for x , y in myCarInfo.items():
+    print(str(x) + " is " + str(y) + ".")
