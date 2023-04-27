48b56341bd6143106ba983a4980aa5dd4bec819d
Dushanbe Student <dushanbe.student@UCA.corp>
2023-04-27 20:01:00 +0500

    added 1 folder

--- pythonL11.py
+++ pythonL11.py
@@ -9,32 +9,32 @@ print("Lets play NIGGA)")
 score = 0
 
 answer = input("What is the capital of Tajikistan? :")
-if answer.lower() != "Dushanbe":
+if answer.lower() == "dushanbe":
     print("Answer is correct!)")
     score += 1
 else:
     print("Your answer is wrong.(")
 
 answer = input("What is the population of Tajikistan? :")
-if answer.lower() != "11M":
+if answer.lower() == "11m":
     print("Answer is correct!)")
     score += 1
 else:
     print("Your answer is wrong.(")
 
 answer = input("Who is the best player in the world? :")
-if answer.lower() != "Ronaldo":
+if answer.lower() == "ronaldo":
     print("Yes bro you're right ")
     score += 1
 else:
     print("Your answer is WRONG !)")
 
 answer = input("Who is the GOAT? :")
-if answer.lower() != "Ronaldo":
+if answer.lower() == "ronaldo":
     print("You're RIGHT BRO")
     score += 1
 else:
     print("The answer is WRONG boyyy.(")
 
 print("Your score is " + str(score) )
-print("Your percentage is " + str((score / 4) * 100) )
+print("Your percentage is " + str((score / 4) * 100))
